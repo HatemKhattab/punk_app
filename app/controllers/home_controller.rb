cf184ca155baf767c6da123dc661fb820be3c29f
Hatem Khattab <hattemkhattab@gmail.com>
2022-11-07 22:34:07 +0100

    searching now working from api

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,23 @@
 class HomeController < ApplicationController
-  
+
+  def search
+    if params[:query].blank? 
+      redirect_to root_path and return
+    else
+      response = ::ApiClients::PunkClient.new.search(params["query"])
+      if response.status != 200
+        @error_msg = "Searching didnt get any result, plz try another search term"
+        return
+      end
+      @arr = response.body
+    end
+  end
+
+
   def index
+     #todo handle eroor från api 
+     #todo handle empty input
+     
   end
   
-  def show
-  end
 end
