61c58469dfef4e1679c24eba7b88292f72105f2f
Hatem Khattab <hattemkhattab@gmail.com>
2022-11-07 23:32:59 +0100

    add show support

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
     if params[:query].blank? 
       redirect_to root_path and return
     else
-      response = ::ApiClients::PunkClient.new.search(params["query"])
+      response = ::ApiClients::PunkClient.new.search(params[:query])
       if response.status != 200
         @error_msg = "Searching didnt get any result, plz try another search term"
         return
@@ -13,11 +13,17 @@ class HomeController < ApplicationController
     end
   end
 
+  def show
+    response = ::ApiClients::PunkClient.new.get_beer(params[:id])
+    @view = response.body.first
+    
+  end
 
   def index
      #todo handle eroor från api 
-     #todo handle empty input
-     
+     #todo handle empty input 
   end
+
+
   
 end
